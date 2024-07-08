41249cfff6dc0b6a6d0cbb4b0120da3a171b11ae
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-08 13:38:34 -0700

    Fix typo on initialize method in Node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,10 +1,10 @@
 class Node
     include Comparable
-    
+
     attr_accessor :left, :right
     attr_reader :data
 
-    def initalize(data)
+    def initialize(data)
         @data = data
         @left = nil
         @right = nil 
