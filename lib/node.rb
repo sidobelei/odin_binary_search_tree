07394c781b52561b526ad857625ecad40a2a6c36
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-08 12:31:08 -0700

    Add spaceship operator method to Node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,4 +1,6 @@
 class Node
+    include Comparable
+    
     attr_accessor :left, :right
     attr_reader :data
 
@@ -6,5 +8,9 @@ class Node
         @data = data
         @left = nil
         @right = nil 
+    end
+
+    def <=>(other)
+        return data <=> other.data
     end
 end
