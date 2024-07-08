4b02c2d3daf75b416e6c5d17b00c08c45bae8e8d
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-08 14:21:13 -0700

    Add pretty_print method to Tree class

--- lib/tree.rb
+++ lib/tree.rb
@@ -22,4 +22,11 @@ class Tree
         node.right = build_tree(array[middle + 1 ... array.length])
         return node 
     end
+
+    # This method was written by another student
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
 end
