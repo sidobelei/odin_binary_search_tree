b4bd7188f50e5c9e7388c141d11f0f79a26da51e
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-11 09:55:01 -0700

    Fix build_tree method's node placement bug

--- lib/tree.rb
+++ lib/tree.rb
@@ -12,14 +12,11 @@ class Tree
         elsif array.length == 0
             return
         end
-        middle = (array.length / 2).round()
-        if array.length % 2 == 0 
-            middle -= 1
-        end
+        middle = (array.length / 2)
         root = array[middle]
         node = Node.new(root)
         node.left = build_tree(array[0...middle])
-        node.right = build_tree(array[middle + 1 ... array.length])
+        node.right = build_tree(array[middle + 1 .. array.length])
         return node 
     end
 
